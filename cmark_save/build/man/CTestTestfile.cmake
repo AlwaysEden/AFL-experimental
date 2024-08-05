52d19ef88fe51b9e48bfd16396090c1b0c197ff8
하정원 <jeongwon@hajeong-won-ui-MacBookPro.local>
2024-08-05 13:03:50 +0900

    Arrange the files

--- cmark_save/build/man/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/s22100141/cmark/man
-# Build directory: /home/s22100141/cmark/build/man
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
